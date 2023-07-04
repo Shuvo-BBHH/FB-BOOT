82f96e503bcc9c5ef42b6def1452ff39a5efa676
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-07-04 11:13:15 +0600

    Update mahdix.py

--- mahdix.py
+++ mahdix.py
@@ -2,4 +2,4 @@ __DEVOLPER__ = '___MAHDI HASAN SHUVO___'
 __FACEBOOK__ = 'MAHDI HASAN'
 __GITHUB__ = 'Shuvo-BBHH'
 # FUCK YOUR DEC .....ENC BY MAHDI HASAN
-print('This tool is Off’);print('Contact Mahdi hasa ')
+print('this tool is off')
